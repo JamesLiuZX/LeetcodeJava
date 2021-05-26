ed57f3f19138a43f895efc545ab8a69e566979ae
ZixinxD <liu.zixin@dhs.sg>
2021-05-26 11:04:21 +0800

    Time: 51 ms (5.37%), Space: 39.2 MB (46.57%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,19 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] results = new int[2];
-        if (nums.length == 2){
-            results[0] = 0;
-            results[1] = 1;
-        }
-        else{
-            for (int i = 0; i < nums.length; i ++){
+        for (int i = 0; i < nums.length; i ++){
             for (int j = i + 1; j < nums.length; j ++){
                 if (nums[i] + nums[j] == target){
-                    results[0] = i;
-                    results[1] = j;
+                    return new int[] {i,j};
                 }
             }
-            }
-        }return results;
+            }throw new IllegalArgumentException("No two sum solution");
+        }
     }   
-}
