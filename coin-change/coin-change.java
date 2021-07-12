f56c93062c65b2c7759c6572b1f7dde9a3d77e05
ZixinxD <liu.zixin@dhs.sg>
2021-07-12 17:16:28 +0800

    Time: 20 ms (46.37%), Space: 38.3 MB (79.26%) - LeetHub

---
+++ coin-change/coin-change.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int [] dp = new int[max];
+        Arrays.fill(dp,max);
+        dp[0] = 0; //it takes 0 coins to have 0 dollars
+        for (int i = 1; i <= dp.length - 1; i++){ //loop to fill up the memo table
+            for (int j = 0; j < coins.length; j++){ //run through each coin
+                //goes down the tree, result basically means (number of coins to reach the  
+                //amount if we deducted the current coin coins[j]) + 1
+                if (i >= coins[j]){ //skip loop if current amount is less than current coin
+                    int result = dp[i - coins[j]] + 1;       
+                    if (result < dp[i]){
+                        dp[i] = result;
+                    }
+                }
+            }
+        }
+        return dp[amount] <= amount ? dp[amount] :-1; //catches case where there is no solution
+    }
+}
+    
